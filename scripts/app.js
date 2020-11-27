72821f634ae7caafa25ae1ded34d2dec7a65c5e2
GeryZlatev <68344068+GeryZlatev@users.noreply.github.com>
2020-11-27 13:32:09 +0200

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,90 @@
+const UserModel = firebase.auth();
+
+const app = Sammy('#root', function() {
+    this.use('Handlebars', 'hbs');
+
+    //GET
+
+    this.get('#/home', function(context) {
+        extendContext(context)
+            .then(function() {
+                this.partial('../templates/home.hbs');
+            });
+    });
+
+    this.get('#/register', function(context) {
+        extendContext(context)
+            .then(function() {
+                this.partial('../templates/register.hbs');
+            });
+    });
+
+    this.get('#/login', function(context) {
+        extendContext(context)
+            .then(function() {
+                this.partial('../templates/login.hbs');
+            });
+    });
+
+
+    //POST
+
+    this.post('#/register', function(context) {
+        const { email, password, repeatPassword } = context.params;
+
+        if (password !== repeatPassword) {
+            let error = 'The passwords must match! Please try again!'
+            errorHandler(error);
+            return;
+        }
+        UserModel.createUserWithEmailAndPassword(email, password)
+            .then((userData) => {
+                this.redirect('#/login');
+            });
+    });
+
+    this.post('#/login', function(context) {
+        const { email, password } = context.params;
+        UserModel.signInWithEmailAndPassword(email, password)
+            .then((userData) => {
+                saveUserData(userData);
+                this.redirect('#/home');
+            })
+            .catch(errorHandler);
+
+    });
+
+
+
+});
+
+app.run('#/home');
+
+
+function extendContext(context) {
+    const user = getUserData();
+    context.isLoggedIn = Boolean(user);
+    context.userEmail = user ? user.email : '';
+
+    return context.loadPartials({
+        header: '../templates/header.hbs',
+    });
+}
+
+function errorHandler(error) {
+    alert(error);
+}
+
+function saveUserData(data) {
+    const { user: { email, uid } } = data;
+    localStorage.setItem('user', JSON.stringify({ email, uid }));
+}
+
+function getUserData() {
+    const user = localStorage.getItem('user');
+    return user ? JSON.parse(user) : null;
+}
+
+function clearUserData() {
+    window.localStorage.removeItem('user');
+}
---
+++ scripts/firebaseConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAmbtZIb9hy0euwE_seyVnzxSLWUiqCFQA",
+    authDomain: "myblog-ae7e7.firebaseapp.com",
+    databaseURL: "https://myblog-ae7e7.firebaseio.com",
+    projectId: "myblog-ae7e7",
+    storageBucket: "myblog-ae7e7.appspot.com",
+    messagingSenderId: "167635353420",
+    appId: "1:167635353420:web:bb4d7c0d480ea1395359e9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
